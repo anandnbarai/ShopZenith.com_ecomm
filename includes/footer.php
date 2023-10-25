001f71f0bbb696667405be3f439e80edcf0285f2
Anand Barai <ab20892@gmail.com>
2023-10-25 19:56:13 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer/Last child -->
 <div class="text-light bg-dark p-2 pt-3 text-center" id="footer">
-    <p>Copyright&#169; 2023 ShopZenith.com - Unleash Your Shopping Potential, All Rights Reserved</p>
+    <p>Copyright&#169; 2023 ShopZenith.com - Unleash Your Shopping Potential, All Rights Reserved (Anand Barai)</p>
 </div>
